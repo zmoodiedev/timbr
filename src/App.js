cdbf7b917c1fd35aec6959e10d72859d868ea8a5
Zach Moodie <dev@zachmoodie.com>
2023-11-23 19:40:23 -0700

    Adding React Router and Pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/header';
-import Marquee from './components/marquee';
-import CardGrid from './components/cardGrid';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Explore from './components/pages/Explore';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Marquee />
-      <CardGrid />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/nav.css'
 
-function Navigation() {
+
+export default function App() {
     return (
         <nav className="navbar">
             <div className="logo">
                 <a href="index.html">Timbr</a>
             </div>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="explore.html">Explore</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about.html">About</Link></li>
+                <li><Link to="/explore.html">Explore</Link></li>
+                <li><Link to="/contact.html">Contact</Link></li>
             </ul>
             <div className="auth">
                 <a href="signup.html">Sign Up</a> | <a href="login.html">Log In</a>
@@ -20,6 +22,3 @@ function Navigation() {
 
     );
 };
-
-
-export default Navigation;
